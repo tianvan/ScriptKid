af4b51357e7d4784348930976ab9379ccb60bc28
Van Tian <vantian@live.com>
2022-05-22 18:35:53 +0800

    refactor(example):  Sync ScriptKidExample naming

--- example/ScriptKidExample/Pages/Error.cshtml.cs
+++ example/ScriptKidExample/Pages/Error.cshtml.cs
@@ -3,7 +3,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace ScriptKitExample.Pages;
+namespace ScriptKidExample.Pages;
 [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
 [IgnoreAntiforgeryToken]
 public class ErrorModel : PageModel
