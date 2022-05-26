f5da599904bbd3757f3fb8c172d831c70c74f442
Van Tian <vantian@live.com>
2022-05-26 22:02:26 +0800

    feat: Dignostice original script

--- src/ScriptKid/ScriptFormatter.cs
+++ src/ScriptKid/ScriptFormatter.cs
@@ -9,6 +9,10 @@ internal class ScriptFormatter : IScriptFormatter
     public string Format(string originalScript)
     {
         SyntaxTree? synaxTree = CSharpSyntaxTree.ParseText(originalScript);
+
+        IEnumerable<Diagnostic>? errors = synaxTree.GetDiagnostics().Where(d => d.Severity == DiagnosticSeverity.Error);
+        if (errors.Any()) throw new ArgumentException($"Invalid script, {string.Join(',', errors.Select(x => x.GetMessage()))}", nameof(originalScript));
+
         CompilationUnitSyntax? root = synaxTree.GetCompilationUnitRoot();
         return root.ToString();
     }
